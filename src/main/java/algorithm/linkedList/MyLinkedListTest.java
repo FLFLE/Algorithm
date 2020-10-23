fbbab94adde327a2b645892bb3587de4769d6dd2
FLFLE <1792236446@qq.com>
2020-10-23 13:15:49 +0800

    identify

--- src/main/java/algorithm/linkedList/MyLinkedListTest.java
+++ src/main/java/algorithm/linkedList/MyLinkedListTest.java
@@ -14,6 +14,7 @@ public class MyLinkedListTest {
         linkedList2.insert_to_tail(40);
         linkedList2.insert_to_tail(60);
         linkedList2.insert_to_tail(80);
+        linkedList2.insert_to_tail(100);
         linkedList2.print();
     }
 }
